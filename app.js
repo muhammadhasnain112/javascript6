1618d68c1a456b5dd03c497a74adabd6ed557211
muhammadhasnain112 <muhammadhasnain12446@gmail.com>
2024-12-06 22:01:01 +0500

    Random Generate Password

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
+function button() {
+    // document.getElementById("message").innerHTML = "Button clicked "
+    let random1 = (Math.random() * 26) + 65
+    let random2 = (Math.random() * 26) + 97
+    let random3 = (Math.random() * 26) + 97
+    let random4 = (Math.random() * 1) + 64
+    let random5 = Math.floor(Math.random() * 10)
+    let random6 = Math.floor(Math.random() * 10)
+    let random7 = Math.floor(Math.random() * 10)
 
-let random1 = (Math.random() * 26) + 65
-let random2 = (Math.random() * 26) + 97
-let random3 = (Math.random() * 26) + 97
-let random4 = (Math.random() * 1)+64
-let random5 = Math.floor(Math.random() * 10)
-let random6 = Math.floor(Math.random() * 10)
-let random7 = Math.floor(Math.random() * 10)
 
+    let check1 = String.fromCharCode(random1)
+    let check2 = String.fromCharCode(random2)
+    let check3 = String.fromCharCode(random3)
+    let check4 = String.fromCharCode(random4)
 
-let check1 = String.fromCharCode(random1)
-let check2 = String.fromCharCode(random2)
-let check3 = String.fromCharCode(random3)
-let check4 = String.fromCharCode(random4)
+    let checking = check1 + check2 + check3 + check4 + random5 + random6 + random7;
+    document.getElementById("generate").innerHTML = `Randomly Generated Password:<br> <strong>${checking}</strong>`;
 
-let checking = check1 + check2 + check3 + check4 + random5 + random6 + random7;
-document.write(`<h1>  Random A Generate Password <br>  <br> ${checking}`)
+}
